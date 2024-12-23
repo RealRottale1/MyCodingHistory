0d3e6c62137192b3d11713e3632232fe4e1caf1c
RealRottale1 <camarkham2@gmail.com>
2024-12-23 10:37:31 -0500

    Finished Java and Python

---
+++ replitHistoryPages/replitHistoryTemplateLoader.js
@@ -0,0 +1,58 @@
+const templateData = {
+    '0': {
+        name: 'Python',
+        icon: 'https://storage.googleapis.com/replit/images/1664475603315_1442b3c69cc612aff6ef60cce0c69328.jpeg',
+        iconLink: 'https://www.python.org/about/',
+        about: 'During the first two years of high school, I attended a Python 1 and Python 2 class. In these classes I not only learned how to program in Python, but I also earned two certifications showing my knowledge of Python.',
+        importance: 'Learning Python helped make the jump from Luau to Java very smooth. Learning Python also made it easier for me to learn other programming languages in general (like JavaScript).',
+        firstImage: 'https://media.licdn.com/dms/image/v2/D4E2DAQGJQFDJu4mnvQ/profile-treasury-document-cover-images_480/profile-treasury-document-cover-images_480/0/1731586502129?e=1735570800&v=beta&t=fQNlz5b3zPCwKhGmhTX4MdCkIcGbSJkvgRhezbcG08c',
+        firstLink: 'https://www.linkedin.com/in/christopher-markham-782645329/details/certifications/1731586515692/single-media-viewer/?type=DOCUMENT&profileId=ACoAAFLawhIBcSM23AneUXqIZLwycDOa7eib0Tc',
+        secondImage: 'https://media.licdn.com/dms/image/v2/D4E2DAQHZfkgClYyjVA/profile-treasury-document-cover-images_480/profile-treasury-document-cover-images_480/0/1731586375921?e=1735570800&v=beta&t=Uv_j9kKvInxF6k05ZEsk_bCIUtXYtW1P5ZISsKxVnak',
+        secondLink: 'https://www.linkedin.com/in/christopher-markham-782645329/details/certifications/1731586382184/single-media-viewer/?profileId=ACoAAFLawhIBcSM23AneUXqIZLwycDOa7eib0Tc',
+    },
+    '1': {
+        name: 'Java',
+        icon: 'https://storage.googleapis.com/replit/images/1664475636706_4242518983fa1131df568c361d7dc030.jpeg',
+        iconLink: 'https://www.java.com/en/download/help/whatis_java.html',
+        about: 'During my junior year of high school, I decided that I wanted to learn other programming languages besides Luau and Python. I decided I would dip my toes into Java. I am by no means an expert at Java, but I am somewhat fluent with its syntax.',
+        importance: 'Learning Java made learning JavaScript extremely easy, as Java and JavaScript share a lot of components. The games to the right are the only two Java games I made (those being Minesweeper and Sudoku).',
+        firstImage: '../images/minesweeper.png',
+        firstLink: 'https://replit.com/@CAMarkham/Minesweeper',
+        secondImage: '../images/sudoku.png',
+        secondLink: 'https://replit.com/@CAMarkham/Sudoku',
+    },
+};
+
+let pageID = window.location.search.replace('?pageID=','');
+if (!templateData[pageID]) {
+    pageID = '0';
+};
+
+const gameTitle = document.getElementById('project_name');
+const codeButton = document.getElementById('code_button');
+const codeIcon = document.getElementById('code_icon');
+const gameImage1 = document.getElementById('image1');
+const gameImage2 = document.getElementById('image2');
+const aboutDescription = document.getElementById('about_description');
+const aboutImportance = document.getElementById('importance_description');
+const playGameButton1 = document.getElementById('image_button1');
+const playGameButton2 = document.getElementById('image_button2');
+
+gameTitle.textContent = templateData[pageID].name;
+codeIcon.src = templateData[pageID].icon;
+gameImage1.src = templateData[pageID].firstImage;
+gameImage2.src = templateData[pageID].secondImage;
+aboutDescription.textContent = templateData[pageID].about;
+aboutImportance.textContent = templateData[pageID].importance;
+
+codeButton.addEventListener('click', function() {
+    window.open(templateData[pageID].iconLink, '_blank');
+});
+
+playGameButton1.addEventListener('click', function() {
+    window.open(templateData[pageID].firstLink, '_blank');
+});
+
+playGameButton2.addEventListener('click', function() {
+    window.open(templateData[pageID].secondLink, '_blank');
+});
--- vsStudioHistoryPages/vsStudioHistoryTemplateLoader.js
+++ vsStudioHistoryPages/vsStudioHistoryTemplateLoader.js
@@ -1,12 +1,12 @@
 const templateData = {
     '0': {
         pointName: 'Early JavaScript',
-        description: 'This time period is between the creation of Dream world I (started on 12/29/2020) and the creation of Car Obby (started on 6/30/2021). This time period is mainly defined by its lag and primitive code (excluding the new stuff I added in after this time period). My most advanced game would have to be Car Obby because of its explosion technology (still very primitive).',
-        importance: 'This time period is important because it played an important role in getting me hooked on Roblox Studio. The only reason I am a good programmer today or even interested in programming is because Roblox Studio was so fun it made me not want to give up even when my code did not work, which allowed me to grow my programming skills.',
-        firstImage: '../images/dreamworldI.png',
-        firstLink: 'https://www.roblox.com/games/6157355653/Dream-world-I',
-        secondImage: '../images/carObby.png',
-        secondLink: 'https://www.roblox.com/games/7028222897/Car-Obby',
+        description: 'As of right now this time period is still on going. This time period started upon the creation of Tic Tac Toe ULTRA.',
+        importance: 'This time period is important for many reasons. The first reason being that this is my first time using a markup language (HTML and CSS). The second reason being that this was my first time ever creating a game from scratch (no pre made engine).',
+        firstImage: '../images/ticTacToeULTRA.png',
+        firstLink: '../ticTacToeCopy/index.html',
+        secondImage: '../images/redBattleUndying.png',
+        secondLink: '../redBattleUndyingCopy/index.html',
     },
 };
 
@@ -20,7 +20,8 @@ const gameImage1 = document.getElementById('game_image1');
 const gameImage2 = document.getElementById('game_image2');
 const aboutDescription = document.getElementById('about_description');
 const aboutImportance = document.getElementById('importance_description');
-const playGameButton = document.getElementById('play_button');
+const playGameButton1 = document.getElementById('play_button1');
+const playGameButton2 = document.getElementById('play_button2');
 
 gameTitle.textContent = templateData[pageID].pointName;
 gameImage1.src = templateData[pageID].firstImage;
@@ -28,6 +29,10 @@ gameImage2.src = templateData[pageID].secondImage;
 aboutDescription.textContent = templateData[pageID].description;
 aboutImportance.textContent = templateData[pageID].importance;
 
-playGameButton.addEventListener('click', function() {
-    window.open(templateData[pageID].playPage, '_blank');
+playGameButton1.addEventListener('click', function() {
+    window.open(templateData[pageID].firstLink, '_blank');
+});
+
+playGameButton2.addEventListener('click', function() {
+    window.open(templateData[pageID].secondLink, '_blank');
 });
